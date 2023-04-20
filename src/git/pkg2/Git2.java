191cdc41a3d2a10479e33bc4b701ca816aeb0340
Gabriel Laulhe <gabriellole@hotmail.com>
2023-04-19 21:47:17 -0300

    git commit

---
+++ src/git/pkg2/Git2.java
@@ -0,0 +1,17 @@
+
+
+package git.pkg2;
+import java.util.Scanner;
+/**
+ *
+ * @author {Gabi}
+ */
+
+public class Git2 {
+    public static Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+    public static void main(String[] args) {
+        
+    }
+
+}
